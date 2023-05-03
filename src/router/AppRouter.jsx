742a49f787dd53e1eb87391f9e6612e3493ac59e
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-05-03 23:43:09 +0300

    feat: update routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import route from "./routes";
-import { Component } from "../components/Component/Component";
+import { Component, Home, Props, State } from "components";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={route.HOME} element={<Component />}>
-        <Route path={route.COMPONENT} />
-      </Route>
+      <Route path={route.HOME} element={<Home />}></Route>
+      <Route path={route.COMPONENT} element={<Component />} />
+      <Route path={route.PROPS} element={<Props />} />
+      <Route path={route.STATE} element={<State />} />
+      <Route path={route.LIFECYCLE} element={<Component />} />
+      <Route path={route.EVENTS} element={<Component />} />
+      <Route path={route.KEY} element={<Component />} />
+      <Route path={route.REFS} element={<Component />} />
+      <Route path={route.ASYNC} element={<Component />} />
+      <Route path={route.DOM} element={<Component />} />
+      <Route path={route.FRAGMENT} element={<Component />} />
+      <Route path={route.MEMO} element={<Component />} />
+      <Route path={route.EFFECT} element={<Component />} />
+      <Route path={route.CONTEXT} element={<Component />} />
+      <Route path={route.ROUTER} element={<Component />} />
+      <Route path={route.FORM} element={<Component />} />
     </Routes>
   );
 };
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,20 @@
 const route = {
-    'HOME' : '/',
-    'COMPONENT' : '/component',
-    'PROPS' : '/props'
-}
+  HOME: "/",
+  COMPONENT: "/component",
+  PROPS: "/props",
+  STATE: "/state",
+  LIFECYCLE: "/lifecycle",
+  EVENTS: "/events",
+  KEY: "/key",
+  REFS: "/refs",
+  ASYNC: "/async",
+  DOM: "/dom",
+  FRAGMENT: "/fragment",
+  MEMO: "/memo",
+  EFFECT: "/effect",
+  CONTEXT: "/context",
+  ROUTER: "/router",
+  FORM: "/form",
+};
 
-export default route
+export default route;
